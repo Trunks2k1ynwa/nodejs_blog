0ea3406965461f9f8087c714e402f9c6d9d0253b
Trunks <Trungthekop123@gmail.com>
2023-04-03 04:07:00 +0700

    ajakdsfkạdsf

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ const newRouter = require("./news");
 const siteRouter = require("./site");
 
 function route(app) {
-           app.get("/", (req, res) => {
-           res.
-           render("home");
+   app.get("/", (req, res) => {
+    res.
+   render("home");
   });
   app.use("/news", newRouter);
   app.use("/", siteRouter);
